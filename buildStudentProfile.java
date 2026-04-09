cdaa021bf7b76904497512b857a41832eb7af661
ycedice <azieldimakalimo@gmail.com>
2026-04-09 23:53:48 +0800

    Create buildStudentProfile.java

---
+++ buildStudentProfile.java
@@ -0,0 +1,23 @@
+private static void
+buildStudentProfile(){
+    System.out.println(" ---Build Student Profile--- ");
+    Student s = pickStudent();
+    if(s==null) return;
+
+    System.out.print(" New Name (ENTER to Skip): ");
+    String name = sc.nextLine().trim();
+    System.out.print(" New Email (ENTER to Skip): ");
+    String email = sc.nextLine.trim();
+    System.out.print(" Program (ENTER to Skip): ");
+    String PROG + sc.nextLine.trim();
+
+    if(!name.is.Empty())
+        s.setName(name);
+    if(!email.is.Empty())
+        s.setEmail(email);
+    if(!prog.is.Empty())
+        s.setProgram(prog);
+
+    System.out.println("\n Profile updated: " + s);
+    pause();
+}
